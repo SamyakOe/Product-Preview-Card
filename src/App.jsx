147fcf31f82da8a48eab9ff821da214d0b47163e
SamyakOe <samyakoe@email.com>
2026-04-19 11:16:28 +0545

    Completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+export default function App() {
+  return (
+    <div className="bg-[#f2ebe3]  justify-center items-center flex flex-col min-h-screen px-4  ">
+      <Card />
+      <div className="mt-4">
+        Challenge By 
+        <a href="https://www.frontendmentor.io?ref=challenge"> Frontend Mentor. </a>
+          Coded by Samyak Dangol
+      </div>
+    </div>
+  );
+}
